5e7bf9b809469e2657399e2810e8cbf826c9469e
moustaphakh <moustaphaelkhatib@gmail.com>
2025-09-23 16:39:46 +0200

    trying out a pygame initialization with said configuration

--- src/config.py
+++ src/config.py
@@ -1,1 +1,5 @@
 #this file contains the constants and settings needed for the game
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+FPS = 60
+BG_COLOR = (30, 30, 40)  #background color
--- src/main.py
+++ src/main.py
@@ -1,1 +1,32 @@
+#imports
+import pygame
 #this is the main code
+from config import SCREEN_WIDTH, SCREEN_HEIGHT, FPS, BG_COLOR #importing constants from config.py
+
+def main():
+    #initialization
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Game Demo")
+    clock = pygame.time.Clock()
+
+    running = True
+    while running:
+        # Handle quit event
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        
+        screen.fill(BG_COLOR)
+
+        #display update
+        pygame.display.flip()
+
+        #framerate
+        clock.tick(FPS)
+
+    pygame.quit()
+#entry point
+if __name__ == "__main__":
+    main()
